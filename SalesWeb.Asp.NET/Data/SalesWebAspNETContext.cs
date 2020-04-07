0d0b3ad55e869a8dd461f6dfdb157d581791ba8b
Lucas Alves de Abreu <lucas.abreu30@etec.sp.gov.br>
2020-04-06 21:25:13 -0300

    CRUD Scaffolding

---
+++ SalesWeb.Asp.NET/Data/SalesWebAspNETContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWeb.Asp.NET.Models
+{
+    public class SalesWebAspNETContext : DbContext
+    {
+        public SalesWebAspNETContext (DbContextOptions<SalesWebAspNETContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWeb.Asp.NET.Models.Department> Department { get; set; }
+    }
+}
--- SalesWeb.Asp.NET/Startup.cs
+++ SalesWeb.Asp.NET/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Asp.NET.Models;
 
 namespace SalesWeb.Asp.NET
 {
@@ -33,6 +35,9 @@ namespace SalesWeb.Asp.NET
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebAspNETContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebAspNETContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

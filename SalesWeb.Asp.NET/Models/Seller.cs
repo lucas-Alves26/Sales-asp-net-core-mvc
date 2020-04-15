c8596f081472dbf9dbae7f0e845ebb706e9930f0
Lucas Alves de Abreu <lucas.abreu30@etec.sp.gov.br>
2020-04-15 20:04:59 -0300

    App locale, number and date formatting

--- SalesWeb.Asp.NET/Models/Seller.cs
+++ SalesWeb.Asp.NET/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWeb.Asp.NET.Models
@@ -7,9 +8,20 @@ namespace SalesWeb.Asp.NET.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)] //Deixa o email com o link para abrir em sua caixa de email padrão
         public string Email { get; set; }
+
+        [Display(Name = "Data de Nascimeto")]
+        [DataType(DataType.Date)] //dd/MM/yyyy
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; } // Data de Nascimento
+
+        [Display(Name ="Salário base")]
+        [DisplayFormat(DataFormatString = "{0:f2}")]// adiciona duas casas decimais
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
--- SalesWeb.Asp.NET/Startup.cs
+++ SalesWeb.Asp.NET/Startup.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWeb.Asp.NET.Models;
@@ -51,6 +53,15 @@ namespace SalesWeb.Asp.NET
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            //Definindo localização como Estados Unidos
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
             //Verifica se o perfil é de desenvovilmento
             if (env.IsDevelopment())
             {

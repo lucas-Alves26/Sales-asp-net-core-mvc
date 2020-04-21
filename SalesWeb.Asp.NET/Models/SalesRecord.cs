cf61ac8fffbba77944e140086fdd9ade2cdc079a
Lucas Alves de Abreu <lucas.abreu30@etec.sp.gov.br>
2020-04-21 19:49:12 -0300

    Implementing simple search

--- SalesWeb.Asp.NET/Models/SalesRecord.cs
+++ SalesWeb.Asp.NET/Models/SalesRecord.cs
@@ -1,14 +1,18 @@
 ﻿using SalesWeb.Asp.NET.Models.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWeb.Asp.NET.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+        [Display(Name = "Data")]
         public DateTime Date { get; set; }
+        [Display(Name = "Valor")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
+        [Display(Name = "Vendedor")]
         public Seller Seller { get; set; }
 
         public SalesRecord()
---
+++ SalesWeb.Asp.NET/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWeb.Asp.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWeb.Asp.NET.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebAspNETContext _context;
+
+        public SalesRecordService(SalesWebAspNETContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return   await result
+                //faz o join entre as tabelas
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                //Ordena por data
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
